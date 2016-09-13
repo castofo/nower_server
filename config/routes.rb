d57b0b111fd39e48d26cf801441ed59c59616838
Ubuntu <ubuntu@ip-172-31-61-29.ec2.internal>
2016-09-13 03:48:37 +0000

    Fixing routes to match 'api' subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, path: '', constraints: lambda { |rq| rq.subdomain.split('.').first == 'api' } do
     namespace :v1 do
       resources :promos
     end
