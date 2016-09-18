232c9dc9fdcd0187656fbc8dfcc5e3b9c91d624f
Santiago Vanegas Gil <savanegasg@gmail.com>
2016-09-18 16:54:51 -0500

    Creating user model and migration files. Including factory

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+require 'faker'
+
+FactoryGirl.define do
+  name = Faker::Name.name
+
+  factory :user do
+    first_name name.split(' ').first
+    last_name name.split(' ').second
+    email Faker::Internet.email name
+    password_digest Faker::Internet.password
+    birthday Faker::Date.between(70.years.ago, 5.years.ago)
+    gender [nil, 'M', 'F'].sample
+  end
+end
