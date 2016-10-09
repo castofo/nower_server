4a98d5b7d7ce32211f7780a5d094aa4f289ab041
Santiago Vanegas Gil <savanegasg@gmail.com>
2016-10-09 15:12:56 -0500

    Including swagger documentation for authorizations controller

--- app/controllers/api/v1/promos_controller.rb
+++ app/controllers/api/v1/promos_controller.rb
@@ -4,18 +4,17 @@ module Api::V1
 
     before_action :set_promo, only: [:show, :update, :destroy]
 
-    swagger_controller :promos, 'Promos'
-
-    swagger_api :index do
-      summary 'Fetches all Promos'
-      notes 'This lists all the promos'
-      response :ok
-      response :unauthorized
-      type :index_response
-    end
+    swagger_controller :promos, 'Promos Management'
 
     swagger_model :index_response do
        property :promos, :array, :required, 'List of promos', { items: { "$ref" => "promo" } }
+    end
+
+    swagger_api :index do
+      summary 'Fetches all Promos'
+      response :ok
+      response :unauthorized
+      type :index_response
     end
 
     # GET /promos
@@ -30,7 +29,7 @@ module Api::V1
       render json: @promo
     end
 
-    swagger_model :promo do
+    swagger_model :create_promo do
       description "A Promo object."
       property :name, :string, :required, 'Name of the promo'
       property :description, :text, :required, 'Description of the promo'
@@ -43,10 +42,11 @@ module Api::V1
 
     swagger_api :create do
       summary 'Creates a new promo'
-      param :body, :body, :promo, :required, 'Promo to be created'
+      param :body, :body, :create_promo, :required, 'Promo to be created'
       response :created
       response :unauthorized
       response :unprocessable_entity
+      type :create_promo
     end
 
     # POST /promos
@@ -60,6 +60,29 @@ module Api::V1
       end
     end
 
+    swagger_model :update_promo do
+      description "A Promo object."
+      property :name, :string, 'New name of the promo'
+      property :description, :text, 'New description of the promo'
+      property :terms, :text, 'New terms and condition for the promo'
+      property :stock, :integer, 'New stock for the promo (available items)'
+      property :price, :double, 'New price of each item related with the promo'
+      property :start_date, :date, 'New start date of the promo'
+      property :end_date, :date, 'New end date of the promo'
+    end
+
+    swagger_api :update do
+      summary 'Updates an existing promo'
+      param :path, :id, :uuid, :required, 'Id of the promo to be updated'
+      param :body, :body, :update_promo, :required, 'Promo values to be changed'
+      response :ok
+      response :bad_request
+      response :unauthorized
+      response :not_found
+      response :unprocessable_entity
+      type :update_promo
+    end
+
     # PATCH/PUT /promos/1
     def update
       if @promo.update(promo_params)
@@ -67,6 +90,15 @@ module Api::V1
       else
         render json: @promo.errors, status: :unprocessable_entity
       end
+    end
+
+    swagger_api :destroy do
+      summary 'Deletes an existing promo'
+      param :path, :id, :uuid, :required, 'Id of the promo to be deleted'
+      response :ok
+      response :unauthorized
+      response :not_found
+      response :unprocessable_entity
     end
 
     # DELETE /promos/1
