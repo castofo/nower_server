87094a3ce7397dbc9e8453e6e09c7322cf57d6f3
Santiago Vanegas Gil <savanegasg@gmail.com>
2016-12-20 00:35:03 -0500

    Configuring mailer options with SMTP for Nower’s Google account

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'puma', '~> 3.0'
 gem 'validates_timeliness', '~> 4.0'
 gem 'bcrypt', '~> 3.1.11'
 gem 'jwt', '~> 1.5.5'
+gem 'figaro', '~> 1.1.1'
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.5.1'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,9 +29,16 @@ Rails.application.configure do
   # Do care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :test
-  host = 'dev.nower.co:3000'
-  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['gmail_username'],
+    password: ENV['gmail_password'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
