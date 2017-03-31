3d51c329af8c316c4dcdb29114f011c634383e7c
Santiago Vanegas Gil <savanegasg@gmail.com>
2017-03-31 16:29:41 -0500

    Creating show method for branch

--- app/controllers/api/v1/branches_controller.rb
+++ app/controllers/api/v1/branches_controller.rb
@@ -1,9 +1,21 @@
 module Api::V1
   class BranchesController < ApplicationController
+    before_action :set_branch, only: [:show]
 
-    # GET branches
+    # GET /branches
     def index
       render json: Branch.all
     end
+
+    # GET /branches/1
+    def show
+      render json: @branch
+    end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_branch
+        @branch = Branch.find(params[:id])
+      end
   end
 end
