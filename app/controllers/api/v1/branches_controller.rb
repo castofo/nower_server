3beab55b26f01b61af9bd1b73a711779dbc8ac15
Santiago Vanegas <savanegasg@gmail.com>
2017-05-16 14:36:59 -0500

    Adding REST endpoints for branches, using proper validations and tests

--- app/controllers/api/v1/branches_controller.rb
+++ app/controllers/api/v1/branches_controller.rb
@@ -1,6 +1,6 @@
 module Api::V1
   class BranchesController < ApplicationController
-    before_action :set_branch, only: [:show]
+    before_action :set_branch, only: [:show, :update, :destroy]
 
     # GET /branches
     def index
@@ -20,10 +20,50 @@ module Api::V1
       render json: @branch
     end
 
+    # POST /branches
+    def create
+      @branch = Branch.new(branch_params)
+
+      if @branch.save
+        render json: @branch, status: :created
+      else
+        render json: @branch.errors, status: :unprocessable_entity
+      end
+    end
+
+    # PATCH/PUT /branches/1
+    def update
+      if @branch.update(branch_params)
+        render json: @branch
+      else
+        render json: @branch.errors, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /branches/1
+    def destroy
+      if @branch.destroy
+        render json: { success: true }
+      else
+        renser json: @branch.errors, status: :unprocessable_entity
+      end
+    end
+
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_branch
         @branch = Branch.find(params[:id])
+      end
+
+      # Only allow a trusted parameter "white list" through.
+      def branch_params
+        params.require(:branch).permit(
+            :name,
+            :latitude,
+            :longitude,
+            :address,
+            :default_contact_info
+        )
       end
   end
 end
